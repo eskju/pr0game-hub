9cbb3ca362cb5e5943d6cad5ed94434deffb3141
Christian Witte <christian.witte@hellgrau.com>
2022-01-27 18:56:01 +0100

    added chart method

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -151,7 +151,7 @@ class PlayerController extends Controller
                 ->where('is_deleted', 0)
                 ->get()
                 ->pluck('id'),
-            'version' => '0.4.1',
+            'version' => '0.4.2',
             'player' => $planet ? $planet->player : []
         ];
     }
