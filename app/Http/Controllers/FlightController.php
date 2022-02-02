24742f0c1a93cf75045bf2c8ca888c753e6ac97b
Christian Witte <christian.witte@hellgrau.com>
2022-02-02 23:36:55 +0100

    added release 1.0.13

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -24,9 +24,7 @@ class FlightController extends Controller
         $flight->outbound_flight_id = $request->get('outbound_flight_id');
         $flight->type = $request->get('type');
         $flight->planet_start_coordinates = $request->get('planet_start_coordinates');
-        $flight->player_start_id = PlayerService::getPlayerByCoordinates($request->get('planet_start_coordinates'));
         $flight->planet_target_coordinates = $request->get('planet_target_coordinates');
-        $flight->player_target_id = PlayerService::getPlayerByCoordinates($request->get('planet_target_coordinates'));
 
         return response([]);
     }
