54ff097386268d3d0008f6308b1924a0f184e4f5
Christian Witte <christian.witte@hellgrau.com>
2022-02-02 23:44:16 +0100

    added release 1.0.13

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -13,14 +13,14 @@ class FlightController extends Controller
     {
         if(!$flight = Flight::query()->where('external_id', $request->get('id'))->where('is_return', $request->get('is_return'))->first()) {
             $flight = new Flight();
-            $flight->external_id = $request->get('id');
-            $flight->is_return = $request->get('is_return');
         }
 
         // create planets if not known
         PlanetService::getPlanetExternalId($request->get('planet_start_coordinates'));
         PlanetService::getPlanetExternalId($request->get('planet_target_coordinates'));
 
+        $flight->external_id = $request->get('external_id');
+        $flight->is_return = $request->get('is_return');
         $flight->outbound_flight_id = $request->get('outbound_flight_id');
         $flight->type = $request->get('type');
         $flight->planet_start_coordinates = $request->get('planet_start_coordinates');
