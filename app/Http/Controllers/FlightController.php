0d939f13d7132a5a18e9618e89bd04517dc10e96
Christian Witte <christian.witte@hellgrau.com>
2022-02-04 09:17:57 +0100

    added release 1.0.14

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -53,9 +53,9 @@ class FlightController extends Controller
             $flight->planet_target_coordinates = $activity->planet_target_coordinates ?? null;
             $flight->player_target_id = Planet::query()->where('coordinates', $activity->planet_target_coordinates ?? null)->first()->player_id ?? null;
             $flight->resources = $activity->resources ?? [];
-            $flight->resources_diff = $activity->is_return ? $this->getDiff((array)($outboundFlight->resources ?? []), (array)($flight->resources ?? [])) : null;
+            $flight->resources_diff = $activity->is_return ? $this->getDiff((array)($outboundFlight->resources ?? $flight->resources ?? []), (array)($flight->resources ?? [])) : null;
             $flight->ships = $activity->ships ?? [];
-            $flight->ships_diff = $activity->is_return ? $this->getDiff((array)($outboundFlight->ships ?? []), (array)($flight->ships ?? [])) : null;
+            $flight->ships_diff = $activity->is_return ? $this->getDiff((array)($outboundFlight->ships ?? $flight->ships ?? []), (array)($flight->ships ?? [])) : null;
             $flight->save();
         }
 
