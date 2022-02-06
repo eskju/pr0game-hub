c7b9ff1c094baee2cfaf5d731206e47f62794223
Christian Witte <christian.witte@hellgrau.com>
2022-02-06 15:24:50 +0100

    track galaxy views

--- app/Http/Controllers/PlanetController.php
+++ app/Http/Controllers/PlanetController.php
@@ -13,11 +13,12 @@ class PlanetController extends Controller
 {
     public function storePlanetId(Request $request)
     {
+        $coordinates = explode(':', $request->get('coordinates'));
+
         if (!$planet = Planet::query()->where('coordinates', $request->get('coordinates'))->first()) {
             $planet = new Planet();
             $planet->player_id = $request->get('player_id');
             $planet->coordinates = $request->get('coordinates');
-            $coordinates = explode(':', $request->get('coordinates'));
             $planet->galaxy = $coordinates[0];
             $planet->system = $coordinates[1];
             $planet->planet = $coordinates[2];
