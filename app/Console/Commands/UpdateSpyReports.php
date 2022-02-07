025edef9b09c3a6a04b1f36e65b7ad804c662d63
Christian Witte <christian.witte@hellgrau.com>
2022-02-07 12:55:40 +0100

    added release 1.0.20

---
+++ app/Console/Commands/UpdateSpyReports.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\SpyReport;
+use App\Services\PlanetService;
+use Illuminate\Console\Command;
+
+class UpdateSpyReports extends Command
+{
+    /**
+     * The name and signature of the console command.
+     * @var string
+     */
+    protected $signature = 'update:spy-reports';
+
+    /**
+     * The console command description.
+     * @var string
+     */
+    protected $description = '';
+
+    /**
+     * Create a new command instance.
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * @return int
+     */
+    public function handle()
+    {
+        $coords = SpyReport::query()
+            ->groupBy('coordinates')
+            ->get();
+
+        $bar = $this->output->createProgressBar(count($coords));
+        foreach ($coords as $coord) {
+            $bar->advance();
+            $report = SpyReport::query()
+                ->where('coordinates', $coord->coordinates)
+                ->whereNotNull('rocket_launchers')
+                ->orderBy('created_at', 'DESC')
+                ->first();
+
+            PlanetService::updatePlanetBySpyReport($report);
+        }
+
+        $bar->finish();
+    }
+}
