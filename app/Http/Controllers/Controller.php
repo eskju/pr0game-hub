c2af3c67a55ea688c724b7315c3068630ecece51
Christian Witte <christian.witte@hellgrau.com>
2022-02-11 08:54:07 +0100

    added release 1.0.27

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -31,7 +31,7 @@ class Controller extends BaseController
         $userRequest->user_id = $user->id;
         $userRequest->ip_address = request()->ip();
         $userRequest->user_agent = request()->userAgent();
-        $userRequest->last_activity_at = Carbon::now();
+        $userRequest->last_activity_at = Carbon::now()->format('Y-m-d H:i:s');
         $userRequest->save();
 
         Auth::login($user);
