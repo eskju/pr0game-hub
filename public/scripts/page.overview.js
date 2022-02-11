73f0e9b89e21c6dea29b03e66a5fa7c6fccdc45e
Christian Witte <christian.witte@hellgrau.com>
2022-02-11 12:27:32 +0100

    added release 1.0.28

--- public/scripts/page.overview.js
+++ public/scripts/page.overview.js
@@ -110,7 +110,7 @@ window.PageOverview = function () {
         });
 
         $('span.fleets').each(function (key, obj) {
-            if ($($(obj).parent()).html().search(/Verbandsangriff/) !== -1) {
+            if (false && $($(obj).parent()).html().search(/Verbandsangriff/) !== -1) {
                 var tmp = $($(obj).parent()).find('.flight.federation');
                 $($(obj).parent()).find('br').remove();
                 $($(obj).parent()).html($($(obj).parent()).html().replace(/<span>(Verbandsangriff|Angreifen)<\/span>/g, ''));
