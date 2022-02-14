9451811ad11332592e4f3539841c2b4aa9fa7fc0
Christian Witte <christian.witte@hellgrau.com>
2022-02-14 17:27:41 +0100

    added release 1.0.32

--- public/scripts/page.fleet.js
+++ public/scripts/page.fleet.js
@@ -174,7 +174,7 @@ window.PageFleet = function () {
 
         $('.table519').each(function (key, obj) {
             if ($(obj).html().search(/Neuer Auftrag:/) !== -1) {
-                $($(obj).find('input[type=text]')).each(function (key, field) {
+                $($(obj).find('input')).each(function (key, field) {
                     if (field.outerHTML.search(/name\=/) !== -1) {
                         shipId = $(field).attr('name').replace(/ship/, '');
                         shipPoints = $this.getPointsPerShip(shipId) * getInt($(field).val());
