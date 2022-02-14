de984f4639cbe69852e592758ffb97043012ca11
Christian Witte <christian.witte@hellgrau.com>
2022-02-14 09:33:07 +0100

    added release 1.0.32

--- public/scripts/util.number-format.js
+++ public/scripts/util.number-format.js
@@ -1,12 +1,18 @@
 window.numberFormat = function (number, toUnit = false) {
     if (toUnit) {
         number = getInt(number);
+        let unit = 'K';
 
         if (number === 0) {
             return '';
         }
 
-        return (Math.round(number / 100) / 10).toFixed(1) + 'K';
+        if(number > 1000000) {
+            number = number / 1000;
+            unit = 'KK';
+        }
+
+        return (Math.round(number / 100) / 10).toFixed(1) + unit;
     }
 
 
