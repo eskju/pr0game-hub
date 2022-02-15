b0f0376ec66bc264f28c707e1a25425840c60cee
Christian Witte <christian.witte@hellgrau.com>
2022-02-15 13:23:07 +0100

    added release 1.0.34

--- public/scripts/util.display-chart.js
+++ public/scripts/util.display-chart.js
@@ -6,6 +6,7 @@ window.displayChart = function (playerId) {
         const ownScore = [];
         const score = [];
         const scoreRelative = [];
+        const scoreDiff = [];
         const scoreBuilding = [];
         const scoreResearch = [];
         const scoreMilitary = [];
@@ -16,6 +17,7 @@ window.displayChart = function (playerId) {
             ownScore.push(obj.own_score);
             score.push(obj.score_max);
             scoreRelative.push(obj.score_relative);
+            scoreDiff.push(obj.score_diff);
             scoreBuilding.push(obj.score_building);
             scoreResearch.push(obj.score_science);
             scoreMilitary.push(obj.score_military);
@@ -94,17 +96,31 @@ window.displayChart = function (playerId) {
                 }
             );
 
-            const myChart2 = new Chart(
+            new Chart(
                 document.getElementById('playerChartBar'),
                 {
-                    type: 'bar',
                     data: {
                         labels: scoreRelative,
                         datasets: [
                             {
+                                type: 'bar',
                                 label: 'Punktedifferenz relativ (in %)',
+                                yAxisID: 'y',
                                 data: scoreRelative,
                                 backgroundColor: function (context) {
+                                    var index = context.dataIndex;
+                                    var value = context.dataset.data[index];
+                                    return value < 0 ? '#ee4d2e' : '#addc8d';
+                                },
+                            },
+                            {
+                                type: 'line',
+                                label: 'Punktedifferenz absolut',
+                                yAxisID: 'y2',
+                                data: scoreDiff,
+                                pointRadius: 0,
+                                borderWidth: 2,
+                                borderColor: function (context) {
                                     var index = context.dataIndex;
                                     var value = context.dataset.data[index];
                                     return value < 0 ? '#ee4d2e' : '#addc8d';
@@ -116,6 +132,14 @@ window.displayChart = function (playerId) {
                         scales: {
                             x: {
                                 display: false
+                            },
+                            y: {
+                                type: 'linear',
+                                position: 'left'
+                            },
+                            y2: {
+                                type: 'logarithmic',
+                                position: 'right'
                             }
                         },
                         plugins: {
