1679b6271dd7f2e9670f43709cbaba3841f99e79
Christian Witte <christian.witte@hellgrau.com>
2022-02-20 05:42:31 +0100

    added release 1.0.37

--- public/scripts/util.get-int.js
+++ public/scripts/util.get-int.js
@@ -1,3 +1,3 @@
 window.getInt = function (intOrString) {
-    return parseInt((intOrString !== null && intOrString !== undefined && intOrString !== ''? intOrString : '0').toString().replace(/\./, ''));
+    return parseInt((intOrString !== null && intOrString !== undefined && intOrString !== ''? intOrString : '0').toString().replace(/\./g, ''));
 }
