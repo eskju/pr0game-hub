cc8d59a0aa8bce4e7911033fed71986d06bbe95d
Christian Witte <christian.witte@hellgrau.com>
2022-02-20 05:46:35 +0100

    added release 1.0.37

--- public/scripts/page.resources.js
+++ public/scripts/page.resources.js
@@ -13,9 +13,9 @@ window.PageResources = function () {
 
             switch (label) {
                 case 'Lagerkapazität':
-                    setValue(ownCoords[0] + '_limit_metal', (getInt($($($(obj).find('td')[1]).find('span')).html()) * 1000).toString());
-                    setValue(ownCoords[0] + '_limit_crystal', (getInt($($($(obj).find('td')[2]).find('span')).html()) * 1000).toString());
-                    setValue(ownCoords[0] + '_limit_deuterium', (getInt($($($(obj).find('td')[3]).find('span')).html()) * 1000).toString());
+                    setValue(ownCoords[0] + '_limit_metal', $this.parseCapacity($($($(obj).find('td')[1]).find('span')).html()));
+                    setValue(ownCoords[0] + '_limit_crystal', $this.parseCapacity($($($(obj).find('td')[2]).find('span')).html()));
+                    setValue(ownCoords[0] + '_limit_deuterium', $this.parseCapacity($($($(obj).find('td')[3]).find('span')).html()));
                     break;
 
                 case 'Pro Tag':
@@ -27,4 +27,18 @@ window.PageResources = function () {
             }
         });
     }
+
+    this.parseCapacity = function(value) {
+        let multiplier = 0;
+
+      if(value.search(/K/) !== -1) {
+          multiplier = 1000;
+      }
+
+      if(value.search(/M/) !== -1) {
+          multiplier = 1000000;
+      }
+
+      return getInt(value) * multiplier;
+    };
 }
