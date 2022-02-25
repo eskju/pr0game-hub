3c67adaffd2730814e770bebf971ee645a3871f9
Christian Witte <christian.witte@hellgrau.com>
2022-02-25 01:57:49 +0100

    added release 1.0.45

--- public/scripts/util.get-player-row-id-style.js
+++ public/scripts/util.get-player-row-id-style.js
@@ -5,9 +5,9 @@ window.getPlayerRowTdStyle = function (obj, ownScore) {
         return {color: getRgb(cGreen)};
     } else if (obj.is_inactive === 1) {
         return {color: getRgb(cCyan)};
-    } else if (getInt(obj.score) < getInt(ownScore) / 5) {
+    } else if (getInt(obj.score) < 5000 && getInt(obj.score) < getInt(ownScore) / 5) {
         return {color: getRgb(cGray)};
-    } else if (getInt(obj.score) > getInt(ownScore) * 5) {
+    } else if (ownScore < 5000 && getInt(obj.score) > getInt(ownScore) * 5) {
         return {color: getRgb(cRed)};
     }
 
@@ -16,25 +16,25 @@ window.getPlayerRowTdStyle = function (obj, ownScore) {
     const enemyAllianceIds = CsvToArray(getValue('filter_ids_enemy_alliances'));
     const enemyIds = CsvToArray(getValue('filter_ids_enemies'));
 
-    for(let i = 0; i < friendAllianceIds.length; i++) {
+    for (let i = 0; i < friendAllianceIds.length; i++) {
         if (friendAllianceIds[i].toString() === (obj.alliance_id || '').toString()) {
             return {color: 'rgb(192, 184, 92)'};
         }
     }
 
-    for(let i = 0; i < friendIds.length; i++) {
+    for (let i = 0; i < friendIds.length; i++) {
         if (friendIds[i].toString() === (obj.id || '').toString()) {
             return {color: 'rgb(192, 184, 92)'};
         }
     }
 
-    for(let i = 0; i < enemyAllianceIds.length; i++) {
+    for (let i = 0; i < enemyAllianceIds.length; i++) {
         if (enemyAllianceIds[i].toString() === (obj.alliance_id || '').toString()) {
             return {color: getRgb(cRed)};
         }
     }
 
-    for(let i = 0; i < enemyIds.length; i++) {
+    for (let i = 0; i < enemyIds.length; i++) {
         if (enemyIds[i].toString() === (obj.id || '').toString()) {
             return {color: getRgb(cRed)};
         }
--- public/scripts/util.get-player-row-style.js
+++ public/scripts/util.get-player-row-style.js
@@ -5,9 +5,9 @@ window.getPlayerRowStyle = function (obj, ownScore) {
         return {background: getRgb(cGreen)};
     } else if (obj.is_inactive === 1) {
         return {background: getRgb(cCyan)};
-    } else if (getInt(obj.score) < ownScore / 5) {
+    } else if (getInt(obj.score) < 5000 && getInt(obj.score) < ownScore / 5) {
         return {background: getRgb(cGray)};
-    } else if (getInt(obj.score) > ownScore * 5) {
+    } else if (ownScore < 5000 && getInt(obj.score) > ownScore * 5) {
         return {background: getRgb(cRed)};
     }
 
@@ -16,25 +16,25 @@ window.getPlayerRowStyle = function (obj, ownScore) {
     const enemyAllianceIds = CsvToArray(getValue('filter_ids_enemy_alliances'));
     const enemyIds = CsvToArray(getValue('filter_ids_enemies'));
 
-    for(let i = 0; i < friendAllianceIds.length; i++) {
+    for (let i = 0; i < friendAllianceIds.length; i++) {
         if (friendAllianceIds[i].toString() === (obj.alliance_id || '').toString()) {
             return {background: 'rgb(192, 184, 92)'};
         }
     }
 
-    for(let i = 0; i < friendIds.length; i++) {
+    for (let i = 0; i < friendIds.length; i++) {
         if (friendIds[i].toString() === (obj.id || '').toString()) {
             return {background: 'rgb(192, 184, 92)'};
         }
     }
 
-    for(let i = 0; i < enemyAllianceIds.length; i++) {
+    for (let i = 0; i < enemyAllianceIds.length; i++) {
         if (enemyAllianceIds[i].toString() === (obj.alliance_id || '').toString()) {
             return {background: getRgb(cRed)};
         }
     }
 
-    for(let i = 0; i < enemyIds.length; i++) {
+    for (let i = 0; i < enemyIds.length; i++) {
         if (enemyIds[i].toString() === (obj.id || '').toString()) {
             return {background: getRgb(cRed)};
         }
