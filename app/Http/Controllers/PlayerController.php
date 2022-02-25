25dc8b08ad11696c23eef3e8de2199261967042f
Christian Witte <christian.witte@hellgrau.com>
2022-02-25 16:44:16 +0100

    added release 1.0.46

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -152,14 +152,6 @@ class PlayerController extends Controller
                     LIMIT 1
                 ) AS `inactive_since`'),
                 DB::raw('ABS(planets.system - ' . (int)$request->get('system') . ') * 100 + ABS(planets.planet - ' . (int)$request->get('planet') . ') AS `distance`'),
-                DB::raw('`players`.`score` - (
-                    SELECT `score`
-                    FROM `log_players`
-                    WHERE `log_players`.`external_id` = `players`.`id`
-                    AND `log_players`.`created_at` <= "' . Carbon::now()->subDay()->format('Y-m-d H:i:s') . '"
-                    ORDER BY `created_at`
-                    DESC LIMIT 1
-                ) AS `diff`'),
                 DB::raw('`players`.`id` AS `player_id`'),
                 DB::raw('`players`.`name`'),
                 DB::raw('`players`.`score`'),
