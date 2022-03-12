c8e7a4d508f1e706d482a9fac0103ef606dec337
Christian Witte <christian.witte@hellgrau.com>
2022-03-12 10:52:10 +0100

    updated search

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -48,6 +48,8 @@ class FlightController extends Controller
                 $outboundFlight = Flight::query()
                     ->where('external_id', $activity->outbound_flight_id)
                     ->where('is_return', '=', 0)
+                    ->where('user_id', $activity->user_id)
+                    ->orderBy('created_at')
                     ->first();
 
                 $inboundIds[] = $activity->external_id ?? null;
