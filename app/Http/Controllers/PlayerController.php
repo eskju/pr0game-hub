8861e83fa05d38d799a817ea56cbd722f25134eb
Christian Witte <christian.witte@hellgrau.com>
2022-03-31 07:15:22 +0200

    added live score

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -223,7 +223,7 @@ class PlayerController extends Controller
                 ->where('is_inactive', 0)
                 ->get()
                 ->pluck('id'),
-            'version' => '1.1.0',
+            'version' => '1.1.1',
             'player' => auth()->user()->player,
             'live_score' => $score,
             'live_score_diff' => $score - $oldScore
