e1eb312d8e16263f33bc0fb2365d2bfd7c5af2c5
Christian Witte <christian.witte@hellgrau.com>
2022-03-31 07:52:45 +0200

    added live score

--- app/Services/PlanetService.php
+++ app/Services/PlanetService.php
@@ -55,16 +55,16 @@ class PlanetService
         $scoreMilitary += (int)$planet->large_transporters * 12;
         $scoreMilitary += (int)$planet->light_hunters * 4;
         $scoreMilitary += (int)$planet->heavy_hunters * 10;
-        $scoreMilitary += (int)$planet->cruisers * 27;
+        $scoreMilitary += (int)$planet->cruisers * 29;
         $scoreMilitary += (int)$planet->battleships * 60;
-        $scoreMilitary += (int)$planet->colony_ships * 30;
-        $scoreMilitary += (int)$planet->recyclers * 16;
+        $scoreMilitary += (int)$planet->colony_ships * 40;
+        $scoreMilitary += (int)$planet->recyclers * 18;
         $scoreMilitary += (int)$planet->spy_drones * 1;
-        $scoreMilitary += (int)$planet->bombers * 75;
-        $scoreMilitary += (int)$planet->solar_satellites * 2;
-        $scoreMilitary += (int)$planet->destroyers * 110;
-        $scoreMilitary += (int)$planet->death_stars * 9000;
-        $scoreMilitary += (int)$planet->battle_cruisers * 70;
+        $scoreMilitary += (int)$planet->solar_satellites * 2.5;
+        $scoreMilitary += (int)$planet->destroyers * 125;
+        $scoreMilitary += (int)$planet->battle_cruisers * 85;
+        $scoreMilitary += (int)$planet->bombers * 90;
+        $scoreMilitary += (int)$planet->death_stars * 10000;
 
         $planet->score_defense = $scoreDefense;
         $planet->score_military = $scoreMilitary;
